398ac0d108d7827bfee4591ea00abf69d1e5db8e
Luis <perezluis167@gmail.com>
2022-11-28 06:30:51 -0500

    Added W4D1 folder

---
+++ W4D1/queue.rb
@@ -0,0 +1,26 @@
+class Queue
+
+    def initialize
+        @queue = Array.new
+
+    end
+
+    def enqueue(el)
+
+        @queue.unshift(el)
+        
+    end
+
+    def dequeue
+
+        @queue.pop
+
+    end
+
+    def peek
+
+        @queue[-1]
+
+    end
+
+end
---
+++ W4D1/stack.rb
@@ -0,0 +1,21 @@
+class Stack
+    def initialize
+      
+      @stack = Array.new
+
+    end
+
+    def push(el)
+    
+      @stack << el
+
+    end
+
+    def pop
+      @stack.pop
+    end
+
+    def peek
+     @stack[-1]
+    end
+  end
