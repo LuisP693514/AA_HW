75ea4ac00059b632e9264824697259472f1c5d15
Luis <perezluis167@gmail.com>
2022-11-28 07:18:38 -0500

    Started graph nodes

---
+++ W4D1/Graph_nodes/GraphNode.rb
@@ -0,0 +1,31 @@
+class GraphNode
+
+    attr_accessor :val, :neighbors
+
+    def initialize(val)
+
+        self.val = val
+        self.neighbors = []
+
+    end
+    
+    def bfs(starting_node, target_value, visited = Set.new)
+
+        return starting_node if starting_node.val == target_value
+
+
+
+    end
+
+end
+
+a = GraphNode.new('a')
+b = GraphNode.new('b')
+c = GraphNode.new('c')
+d = GraphNode.new('d')
+e = GraphNode.new('e')
+f = GraphNode.new('f')
+a.neighbors = [b, c, e]
+c.neighbors = [b, d]
+e.neighbors = [a]
+f.neighbors = [e]
